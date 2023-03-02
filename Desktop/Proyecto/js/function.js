ea06a7573e58fa90d1277be7bdb6e46fe54e8044
Emiliano Severino Bersais <emilianoseverino@gmail.com>
2023-03-01 21:58:02 -0300

    Primer carga web CV

---
+++ Desktop/Proyecto/js/function.js
@@ -0,0 +1,9 @@
+function wp() {
+    window.open("https://wa.me/5492216828913");
+  }
+
+function bienvenida() {alert ("Gracias por visitar mi CV, espero sea de tu agrado. Cualquier duda o consulta, quedo a tu disposicion.")}
+
+function wp2() {
+  window.open("https://wa.me/5492216828913");
+}
